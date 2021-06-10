8fded40bd82032694ab8f8e5cde058813cabf400
akahito1006 <nekodora5k@gmail.com>
2021-06-11 06:47:22 +0900

    part2

--- part1/codes.js
+++ part1/codes.js
@@ -36,3 +36,43 @@ console.log(hoursPerDay);
 const hoursPerDay = 24;
 const minutesPerHour = 60;
 const secondsPerMinute = 60;
+
+let positiveInteger = 42;
+let negativeInteger = -42;
+let largeDecimas = 42.42424242;
+
+console.log(typeof positiveInteger);
+console.log(typeof negativeInteger);
+console.log(typeof largeDecimas);
+
+console.log(0.2+0.2);
+// 0.4
+
+console.log(0.1+0.2);
+// 0.30000000000000004
+
+// 小数点の問題を避けるには、以下のようにする
+let price = 25.99;
+// ↓
+let price = 2599;
+
+let userIsSignedIn = true;
+let userIsAdmin = false;
+
+console.log(typeof userIsSignedIn);
+console.log(typeof userIsAdmin);
+
+let firstName = 'Will';
+let lastName = 'Alexander';
+
+console.log(firstName + ' ' + lastName);
+
+let episodeTitle = 'the Beginning';
+let episodeDuration = 60;
+let hasBeenWatched = false;
+
+let a = true;
+let b = 1;
+let c = "1";
+
+console.log(a + c + b);

7a15dc1b759c5714d8ea4bc0ab7e24cb79ded4dd
akahito1006 <nekodora5k@gmail.com>
2021-06-11 06:20:12 +0900

    codes

---
+++ part1/codes.js
@@ -0,0 +1,30 @@
+let totalCDs = 67;
+let totalVinyls = 37;
+
+totalCDs ++;
+
+let totalMusic = totalCDs + totalVinyls;
+
+console.log(totalMusic);
+
+let costPerProduct = 20;
+let numberOfProducts = 5;
+ 
+let totalCost = costPerProduct * numberOfProducts;
+ 
+let averageCostPerProduct = totalCost / numberOfProducts;
+
+console.log(totalCost);
+
+let numberOfCats = 2;
+
+numberOfCats *= 6;
+console.log(numberOfCats);
+
+numberOfCats /= 3;
+console.log(numberOfCats);
+
+let episodeTime = 45;
+let commercialTime = 5;
+
+let totalShowTime = (episodeTime + commercialTime) * numberOfSeasons * numberOfEpisodes;

769351fa30ba70be0c01237c18f8647647ea2151
akahito1006 <nekodora5k@gmail.com>
2021-06-19 23:58:34 +0900

    unit test

--- part1/codes.js
+++ part1/codes.js
@@ -452,8 +452,8 @@ describe('getWordCount()', function() {
   it('should find four words', function() {
     expect(getWordCount('I have four words').to.equal(4));
   });
-  it('other test', function() {
-    expect(getOtherThing('other result').to.equal(1));
+  it('should find no words', function() {
+    expect(getWordCount('   ').to.equal(0));
   });
 });
 

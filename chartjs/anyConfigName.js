51f7f155713e7727b3372e4658ba09f1ab90a464
akahito1006 <nekodora5k@gmail.com>
2021-10-03 21:16:21 +0900

    pluginTooltips配列

--- chartjs/anyConfigName.js
+++ chartjs/anyConfigName.js
@@ -6,6 +6,26 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
 */}
 
+Chart.pluginService.register({
+  beforeRender: function (chart) {
+    // chart tooltipはチャート1つにつき1つまでしか使えない
+    chart.pluginTooltips = [];
+    // [].forEach(function () {}) 与えられた関数を配列の各要素に対して1度ずつ実行する
+    // 下記、i = 0 、 datasets[0]に対して処理を実行する
+    chart.config.data.datasets.forEach((dataset, i) => {
+      chart.getDatasetMeta(i).data.forEach((sector,j) => {
+        chart.pluginTooltips.push(new Chart.Tooltip({
+          _chart: chart.chart,
+          _chartInstance: chart,
+          _data:  chart.data,
+          _options: chart.options.tooltips,
+          _active: [sector]
+        }, chart));
+      })
+    })
+  }
+});
+
 // data and config
 const labels = [
   'January',
